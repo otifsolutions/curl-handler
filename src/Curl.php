7a002468683495ac891388590856442eef33eeb9
Junaid Maqsood <junaidmaqsood@live.com>
2021-04-01 13:48:19 +0500

    DOMXml Support Added. Minor adjustments to make code cleaner

--- src/Curl.php
+++ src/Curl.php
@@ -2,6 +2,7 @@
 
 namespace OTIFSolutions\CurlHandler;
 use OTIFSolutions\CurlHandler\Exceptions\CurlException;
+use OTIFSolutions\CurlHandler\Helpers\Helper;
 
 /**
  * Class Curl
@@ -26,6 +27,7 @@ class Curl
             public function __construct(){
                 $this->curl = curl_init();
             }
+
             public function __get($method): object
             {
                 if ($method === 'GET'){
@@ -47,23 +49,23 @@ class Curl
             }
             /**
              * @param mixed $func
-             * @param mixed $arguement
+             * @param mixed $arguments
              *
              * @return mixed
              */
-            public function __call($func,$arguement)
+            public function __call($func, $arguments)
             {
                 if($func === 'url'){
-                    return $this->url($arguement);
+                    return $this->url($arguments);
                 }
                 elseif($func === 'header'){
-                    return $this->header($arguement);
+                    return $this->header($arguments);
                 }
                 elseif($func === 'body'){
-                    return $this->body($arguement);
+                    return $this->body($arguments);
                 }
                 elseif ($func === 'params'){
-                    return $this->params($arguement);
+                    return $this->params($arguments);
                 }
                 elseif ($func === 'execute'){
                     return $this->execute();
@@ -80,7 +82,7 @@ class Curl
              */
             private function url($url): object
             {
-                if(isset($url[0]) && gettype($url[0]) === 'string'){
+                if(isset($url[0]) && is_string($url[0])){
                     $this->url = implode($url);
                 }
                 else{
@@ -95,9 +97,7 @@ class Curl
              */
             private function header($headers): object
             {
-
-                if(isset($headers[0]) && gettype($headers[0]) === 'array'){
-                    $this->headers;
+                if(isset($headers[0]) && is_array($headers[0])){
                     if(count($headers[0])>0){
                         $this->headers = $headers[0];
                     }
@@ -115,14 +115,18 @@ class Curl
              */
             private function body($bodies): object
             {
-                if(isset($bodies[0]) && gettype($bodies[0]) === 'array'){
+                if(isset($bodies[0]) && is_array($bodies[0])){
                     $this->bodies;
                     if(count($bodies[0])>0){
-                        $this->bodies = $bodies[0];
+                        $this->bodies = json_encode($bodies[0]);
                     }
                 }
+                elseif(isset($bodies[0]) && is_string($bodies[0])){
+                    $this->bodies;
+                    $this->bodies = $bodies[0];
+                }
                 else{
-                    $this->errors = ['message'=>'Body data must be an array'];
+                    $this->errors[] = 'Body data must be an array or string';
                 }
                 return $this;
             }
@@ -146,28 +150,36 @@ class Curl
                 return $this;
             }
 
+            /**
+             * @return array
+             */
             private function execute() : array
             {
-                if(count($this->errors) ==0){
+                if(count($this->errors) === 0){
                     curl_setopt_array ( $this->curl, [
                         CURLOPT_URL => $this->url.($this->params?'?'.$this->params:''),
                         CURLOPT_RETURNTRANSFER => true,
                         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
                         CURLOPT_CUSTOMREQUEST => $this->method,
                         CURLOPT_HTTPHEADER => $this->headers ?? [],
-                        CURLOPT_POSTFIELDS => $this->bodies ? json_encode($this->bodies) : [],
+                        CURLOPT_POSTFIELDS => $this->bodies,
                     ]);
                     $response = curl_exec($this->curl);
-                        if (curl_error($this->curl)){
-                            $this->errors[] = 'Internal error occured';
-                            throw new CurlException($this->errors);
-                        }
-                    return json_decode($response,true,512,JSON_THROW_ON_ERROR);
+                    if (curl_error($this->curl)){
+                        $this->errors[] = 'Internal error occured';
+                        throw new CurlException($this->errors);
+                    }
+                    if (Helper::isJson($response))
+                        return json_decode($response,true,512,JSON_THROW_ON_ERROR);
+
+                    return Helper::isDOMDocument($response) ? Helper::domToArray($response) : [
+                        'body' => $response
+                    ];
                 }
-                else{
-                    throw new CurlException($this->errors);
-                }
+
+                throw new CurlException($this->errors);
             }
+
             public function __destruct(){
                 curl_close($this->curl);
             }
---
+++ src/Helpers/Helper.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace OTIFSolutions\CurlHandler\Helpers;
+
+/**
+ * Class Helper
+ * @package OTIFSolutions\CurlHandler\Helpers
+ */
+class Helper
+{
+    /**
+     * @param $string
+     * @return bool
+     */
+    public static function isJson($string): bool
+    {
+        json_decode($string);
+        return (json_last_error() == JSON_ERROR_NONE);
+    }
+
+    /**
+     * @param $string
+     * @return bool
+     */
+    public static  function isDomDocument($string): bool
+    {
+        $xml = new \DOMDocument();
+        return $xml->loadXML($string) !== FALSE;
+    }
+
+    /**
+     * @param $node
+     * @return array
+     */
+    public static  function domToArray($node): array {
+        $output = [];
+        if (is_string($node)){
+            $xml = new \DOMDocument();
+            $xml->loadXML($node);
+            $output[$xml->documentElement->tagName] = self::domToArray($xml->documentElement);
+            return $output;
+        }
+        switch ($node->nodeType) {
+            case XML_CDATA_SECTION_NODE:
+                $output['@cdata'] = trim($node->textContent);
+                break;
+
+            case XML_TEXT_NODE:
+                $output = trim($node->textContent);
+                break;
+
+            case XML_ELEMENT_NODE:
+                // for each child node, call the covert function recursively
+                for ($i = 0, $m = $node->childNodes->length; $i < $m; $i++) {
+                    $child = $node->childNodes->item($i);
+                    $v = self::domToArray($child);
+                    if (isset($child->tagName)) {
+                        $t = $child->tagName;
+
+                        // assume more nodes of same kind are coming
+                        if (!array_key_exists($t, $output)) {
+                            $output[$t] = [];
+                        }
+                        $output[$t][] = $v;
+                    } else {
+                        //check if it is not an empty text node
+                        if ($v !== '') {
+                            $output = $v;
+                        }
+                    }
+                }
+
+                if (is_array($output)) {
+                    // if only one node of its kind, assign it directly instead if array($value);
+                    foreach ($output as $t => $v) {
+                        if (is_array($v) && count($v) === 1) {
+                            $output[$t] = $v[0];
+                        }
+                    }
+                    if (count($output) === 0) {
+                        //for empty nodes
+                        $output = '';
+                    }
+                }
+
+                // loop through the attributes and collect them
+                if ($node->attributes->length) {
+                    $a = [];
+                    foreach ($node->attributes as $attrName => $attrNode) {
+                        $a[$attrName] = (string)$attrNode->value;
+                    }
+                    // if its an leaf node, store the value in @value instead of directly storing it.
+                    if (!is_array($output)) {
+                        $output = ['@value' => $output];
+                    }
+                    $output['@attributes'] = $a;
+                }
+                break;
+        }
+
+        return $output;
+    }
+}
