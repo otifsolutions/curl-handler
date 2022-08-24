506ed717a19a549d2c587cb2e1508a178bd8a014
Junaid Maqsood <junaidmaqsood@live.com>
2022-08-24 13:23:56 +0500

    Cookie Support Added

--- src/Curl.php
+++ src/Curl.php
@@ -11,21 +11,24 @@ use OTIFSolutions\CurlHandler\Helpers\Helper;
 class Curl
 {
     /**
+     * @param array $cookies
      * @return object
      */
-    public static function Make():object
+    public static function Make(array &$cookies = []):object
     {
-        return new class(){
-            private $url ;
-            private $bodies ;
-            private $headers ;
-            private $params ;
-            private $method ;
-            private $curl;
-            private $errors=[];
+        return new class($cookies){
+            private string $url ;
+            private mixed $bodies ;
+            private array $headers ;
+            private mixed $params ;
+            private string $method ;
+            private mixed $curl;
+            private array $errors = [];
+            private array $cookies;
 
-            public function __construct(){
+            public function __construct(&$cookies){
                 $this->curl = curl_init();
+                $this->cookies = &$cookies;
             }
 
             public function __get($method): object
@@ -53,34 +56,37 @@ class Curl
              *
              * @return mixed
              */
-            public function __call($func, $arguments)
+            public function __call(mixed $func, mixed $arguments)
             {
-                if($func === 'url'){
+                if ($func === 'url') {
                     return $this->url($arguments);
                 }
-                elseif($func === 'header'){
+
+                if ($func === 'header') {
                     return $this->header($arguments);
                 }
-                elseif($func === 'body'){
+
+                if ($func === 'body') {
                     return $this->body($arguments);
                 }
-                elseif ($func === 'params'){
+
+                if ($func === 'params') {
                     return $this->params($arguments);
                 }
-                elseif ($func === 'execute'){
+
+                if($func === 'execute') {
                     return $this->execute();
                 }
-                else{
-                    $this->errors[] = $func.'()'.' is not defined function';
-                    return $this;
-                }
+
+                $this->errors[] = $func.'()'.' is not defined function';
+                return $this;
             }
             /**
              * @param mixed $url
              *
              * @return object
              */
-            private function url($url): object
+            private function url(mixed $url): object
             {
                 if(isset($url[0]) && is_string($url[0])){
                     $this->url = implode($url);
@@ -95,7 +101,7 @@ class Curl
              *
              * @return object
              */
-            private function header($headers): object
+            private function header(mixed $headers): object
             {
                 if(isset($headers[0]) && is_array($headers[0])){
                     if(count($headers[0])>0){
@@ -113,16 +119,14 @@ class Curl
              *
              * @return object
              */
-            private function body($bodies): object
+            private function body(mixed $bodies): object
             {
                 if(isset($bodies[0]) && is_array($bodies[0])){
-                    $this->bodies;
                     if(count($bodies[0])>0){
-                        $this->bodies = json_encode($bodies[0]);
+                        $this->bodies = json_encode($bodies[0], JSON_THROW_ON_ERROR);
                     }
                 }
                 elseif(isset($bodies[0]) && is_string($bodies[0])){
-                    $this->bodies;
                     $this->bodies = $bodies[0];
                 }
                 else{
@@ -135,10 +139,9 @@ class Curl
              *
              * @return object
              */
-            private function params($params): object
+            private function params(mixed $params): object
             {
-                if(isset($params[0]) && gettype($params[0]) === 'array'){
-                    $this->params;
+                if(isset($params[0]) && is_array($params[0])){
                     if(count($params[0])>0){
                         $this->params = http_build_query($params[0]);
                     }
@@ -151,10 +154,9 @@ class Curl
             }
 
             /**
-             * @param array $cookies
              * @return array
              */
-            private function execute(array &$cookies = []) : array
+            private function execute() : array
             {
                 if(count($this->errors) === 0){
                     curl_setopt_array ( $this->curl, [
@@ -164,9 +166,9 @@ class Curl
                         CURLOPT_CUSTOMREQUEST => $this->method,
                         CURLOPT_HTTPHEADER => $this->headers ?? [],
                         CURLOPT_POSTFIELDS => $this->bodies,
-                        CURLOPT_HEADERFUNCTION => function($ch , $headerLine) use (&$cookies){
-                            if (preg_match('/^Set-Cookie:\s*([^;]*)/mi', $headerLine, $cookie) == 1) {
-                                $cookies[] = $cookie;
+                        CURLOPT_HEADERFUNCTION => function($ch , $headerLine){
+                            if (preg_match('/^Set-Cookie:\s*([^;]*)/mi', $headerLine, $cookie) === 1) {
+                                $this->cookies[] = $cookie;
                             }
                             return strlen($headerLine);
                         }
@@ -176,8 +178,9 @@ class Curl
                         $this->errors[] = 'Internal error occured';
                         throw new CurlException($this->errors);
                     }
-                    if (Helper::isJson($response))
-                        return json_decode($response,true,512,JSON_THROW_ON_ERROR);
+                    if (Helper::isJson($response)) {
+                        return json_decode($response, true, 512, JSON_THROW_ON_ERROR);
+                    }
 
                     return Helper::isDOMDocument($response) ? Helper::domToArray($response) : [
                         'body' => $response
